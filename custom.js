83dca721b46745138cb007724fde150d2b12dc1e
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-09 10:46:12 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/update/230315/bg_007_interworld.mp4";
+        document.getElementById('vd').load();
+    }
+});
